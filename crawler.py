55237ab6cfcc5039643cb87df97dbb085c04187f
mievenfall <mngoc2603@gmail.com>
2024-04-22 00:03:29 -0700

    initial commit

---
+++ crawler.py
@@ -0,0 +1,101 @@
+#-------------------------------------------------------------------------
+# AUTHOR: Evelyn Vu
+# FILENAME: crawler.py
+# SPECIFICATION: Crawl the CS website until the Permanent Faculty page is found
+# FOR: CS 4250- Assignment #4
+# TIME SPENT: 3
+#-----------------------------------------------------------*/
+
+
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+import re 
+from collections import deque 
+
+
+def connectDataBase():
+
+    # Create a database connection object using pymongo
+    DB_HOST = "localhost"
+    DB_PORT = 27017
+
+    try:
+        client = MongoClient(host=DB_HOST, port=DB_PORT)
+        db = client.corpus
+        return db
+    
+    except Exception as e:
+        print("Connect database error: " + str(e))
+
+
+def updateDataBase(url, html):
+    page = pages.find_one({'_id': url})
+    if (page):
+        pages.delete_one({'_id': url})       
+        pages.insert_one({'_id': url, 'text': str(html)})
+
+    else:
+        pages.insert_one({'_id': url, 'text': str(html)})
+
+
+def crawlerThread(frontier: deque):
+
+    # stores visited urls
+    visited = set()
+
+    # while not frontier.done() do
+    while len(frontier) > 0:
+         
+        # url <— frontier.nextURL()
+        url = frontier.popleft()
+        visited.add(url)
+
+        # html <— retrieveURL(url)  
+        try:
+            html = urlopen(url)
+        except HTTPError as e:
+            print("Retrieve URL error: " + str(e))
+            visited.add(url)
+            continue
+        except URLError as e:
+            print("Retrieve URL error: " + str(e))
+            visited.add(url)
+            continue
+            
+        html = BeautifulSoup(html, 'html.parser')
+
+        # update url in db or insert url if not in db yet
+        updateDataBase(url, html)
+
+       
+        # if target_page (html) 
+        if (html.find('h1', string=re.compile('(Permanent Faculty)+'))): # crawler found Permanent Faculty
+
+            # clear_frontier()
+            frontier.clear()
+        
+        else:
+
+            # get all urls in html
+            anchors = html.find_all('a')
+            urls = []
+            for anchor in anchors:
+                href = anchor.get('href')
+                urls.append(urljoin(url, href))
+
+            # for each not visited url in parse (html) do
+            for url in urls:
+                if url not in visited:
+
+                    # frontier.addURL(url)
+                    frontier.append(url)
+                    visited.add(url)
+
+
+db = connectDataBase()
+pages = db.pages
+crawlerThread(deque(["https://www.cpp.edu/sci/computer-science/"]), pages)
---
+++ parser.py
@@ -0,0 +1,94 @@
+#-------------------------------------------------------------------------
+# AUTHOR: Evelyn Vu
+# FILENAME: parser.py
+# SPECIFICATION: Parse faculty members' name, title, office, phone, email, and website, and persist this data in MongoDB
+# FOR: CS 4250- Assignment #4
+# TIME SPENT: 2.5
+#-----------------------------------------------------------*/\
+
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+import re
+
+def connectDataBase():
+
+# Create a database connection object using pymongo
+    DB_HOST = "localhost"
+    DB_PORT = 27017
+
+    try:
+        client = MongoClient(host=DB_HOST, port=DB_PORT)
+        db = client.corpus
+        return db
+    
+    except Exception as e:
+        print("Connect database error: " + str(e))
+
+
+def updateDatabase(doc):
+    currProf = professors.find_one({"_id": doc["name"]})
+    if (currProf):
+        professors.update_one({"_id": doc["name"]}, {"$set": doc})
+    else:
+        professors.insert_one(doc)
+
+
+
+def parser():
+
+    # use the target page URL to find the Permanent Faculty page in the database
+    url = "https://www.cpp.edu/sci/computer-science/faculty-and-staff/permanent-faculty.shtml"
+    page = pages.find_one({"_id": url})
+    html = page.get('text')
+
+    # create beautiful soup object
+    bs = BeautifulSoup(html, 'html.parser')
+    
+    # professors info is under text-images section -> clearfix div -> h2 and p
+    # but not all clearfix div have info
+    # get section -> h2 containing name -> div for container
+    section = bs.find('section', {'class':{'text-images'}})
+    headers = section.find_all('h2')
+    containers = []
+    for i in headers:
+        containers.append(i.parent)
+
+# for each professor
+    for prof in containers:
+        # get name
+        name = prof.h2.get_text().strip()
+
+        # get title
+        title = prof.find("strong", string=re.compile('(Title){1,1}')).next_sibling.get_text()
+        title = title.strip(": ").strip()
+
+        # get office
+        office = prof.find("strong", string=re.compile('(Office){1,1}')).next_sibling.get_text()
+        office = office.strip(":").strip()
+
+        # get email
+        email = prof.find('a', {'href': re.compile("^(mailto:)")}).get('href')
+        email = email.split(":")[1]
+
+        #get website
+        website = prof.find('a', {'href': re.compile(r"^https?:\/\/www\.cpp\.edu\/faculty")}).get('href')
+
+
+        # create professor document
+        doc = {
+            "_id": name,
+            "name": name,
+            "title": title,
+            "office": office,
+            "email": email,
+            "website": website
+        }
+
+        # update doc in db or insert url if not in db yet
+        updateDatabase(doc)
+
+
+db = connectDataBase()
+professors = db.professors
+pages = db.pages
+parser()
